7f707eee9d5cbbcb55c758e3014483e65ff0c3e4
LuisMunar <luis.emunarr@gmail.com>
2024-04-04 21:59:04 -0500

    Video 16 ready

--- src/main/java/com/example/api/controllers/UserController.java
+++ src/main/java/com/example/api/controllers/UserController.java
@@ -7,14 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.api.models.User;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
   @GetMapping({""})
-  public Map<String, Object> getUser() {
-    Map<String, Object> body = new HashMap<>();
-    body.put("name", "John Doe");
-    body.put("email", "john.code@mail.com");
+  public Map<String, User> getUser() {
+    User user = new User("John Doe", "john.doe@mail.com");
+    Map<String, User> body = new HashMap<>();
+    body.put("user", user);
     return body;
   }
 }
