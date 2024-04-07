dcd7e43eeb88607023e99b0d11ddad50f8379839
LuisMunar <luis.emunarr@gmail.com>
2024-04-07 11:54:55 -0500

    Video 40 ready

--- src/main/java/com/example/api/ApiApplication.java
+++ src/main/java/com/example/api/ApiApplication.java
@@ -3,10 +3,13 @@ package com.example.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		// Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(ApiApplication.class, args);
 	}
 }
--- src/main/java/com/example/api/controllers/UserController.java
+++ src/main/java/com/example/api/controllers/UserController.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +19,12 @@ import com.example.api.models.dto.UserDto;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+  @Autowired
+  private Environment environment;
+
+  // @Value("${ENV_MESSAGE}")
+  // private String envMessage;
+
   @GetMapping({"/with-body"})
   public Map<String, User> getUserWithBody() {
     User user = new User("John Doe", "john.doe@mail.com");
@@ -43,5 +52,10 @@ public class UserController {
   @PostMapping({"/create-user"})
   public User createUser(@RequestBody User user) {
     return user;
+  }
+
+  @GetMapping({"/get-environment"})
+  public String createUser() {
+    return environment.getProperty("config.message", "Default regardless of environment");
   }
 }
